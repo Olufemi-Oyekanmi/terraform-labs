3b771370499b966c5488dd743d445049cfc31918
OyekanmiOlufemi <Oyekanmi.Admin@axamansard.com>
2021-03-03 10:23:51 +0100

    Created terraform variables

---
+++ nsgs.tf
@@ -0,0 +1,106 @@
+resource "azurerm_resource_group" "nsgs" {
+    name = "NSGs"
+    location = "${var.loc}"
+    tags = "${var.tags}"  
+}
+
+resource "azurerm_network_security_group" "resource_group_default" {
+    name = "ResourceGroupDefault"
+    resource_group_name = "${azurerm_resource_group.nsgs.name}"
+    location = "${var.loc}"
+    tags = "${azurerm_resource_group.nsgs.tags}"  
+}
+
+resource "azurerm_network_security_rule" "AllowSSH" {
+  name = "AllowSSH"
+  resource_group_name = "${azurerm_resource_group.nsgs.name}"
+  network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+  location = "${azurerm_resource_group.nsgs.location}"
+  priority = 1010
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = 22
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+}
+
+resource "azurerm_network_security_rule" "AllowHTTP" {
+  name = "AllowHTTP"
+  resource_group_name = "${azurerm_resource_group.nsgs.name}"
+  network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+  location = "${azurerm_resource_group.nsgs.location}"
+  priority = 1020
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = 80
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+}
+
+resource "azurerm_network_security_rule" "AllowHTTPS" {
+  name = "AllowHTTPS"
+  resource_group_name = "${azurerm_resource_group.nsgs.name}"
+  network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+  location = "${azurerm_resource_group.nsgs.location}"
+  priority = 1020
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = 443
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+}
+
+resource "azurerm_network_security_rule" "AllowSQLServer" {
+  name = "AllowSQLServer"
+  resource_group_name = "${azurerm_resource_group.nsgs.name}"
+  network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+  location = "${azurerm_resource_group.nsgs.location}"
+  priority = 1443
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = 443
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+}
+
+resource "azurerm_network_security_rule" "AllowRDP" {
+  name = "AllowRDP"
+  resource_group_name = "${azurerm_resource_group.nsgs.name}"
+  network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+  location = "${azurerm_resource_group.nsgs.location}"
+  priority = 1443
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = 443
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+}
+
+resource "azurerm_network_security_group" "nic_ubuntu" {
+  name="nic_ubuntu"
+  location = "${azurerm_resource_group.nsgs.location}"
+  tags = "${azurerm_resource_group.nsgs.tags}"
+
+  security_rule = [ {
+    access = "Allow"
+    description = "The SSH rule to nic_ubuntu"
+    destination_address_prefix = "*"
+    destination_port_range = "22"
+    direction = "Inbound"
+    name = "SSH"
+    priority = 100
+    protocol = "Tcp"
+    source_address_prefix = "*"
+    source_port_range = "*"
+  } ]
+}

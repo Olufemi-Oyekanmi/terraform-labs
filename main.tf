a308e3ba4db2b10002a91f615589e748cdb3bf02
OyekanmiOlufemi <Oyekanmi.Admin@axamansard.com>
2021-03-03 11:47:43 +0100

    saved

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    version = "~> 1.33.1"  
+  features {}
 }
 
 resource "azurerm_resource_group" "lab1" {

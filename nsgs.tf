9f300ab7628f5e908c1c0050894db6f87f376524
OyekanmiOlufemi <Oyekanmi.Admin@axamansard.com>
2021-03-03 10:31:30 +0100

    edited the terraform location var

--- nsgs.tf
+++ nsgs.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "nsgs" {
 resource "azurerm_network_security_group" "resource_group_default" {
     name = "ResourceGroupDefault"
     resource_group_name = "${azurerm_resource_group.nsgs.name}"
-    location = "${var.loc}"
+    location = "${azure_resource_group.nsgs.loc}"
     tags = "${azurerm_resource_group.nsgs.tags}"  
 }
 

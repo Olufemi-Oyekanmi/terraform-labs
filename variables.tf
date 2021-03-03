27678242e4e63bbfca8a387d1151ddc5c8a345d2
OyekanmiOlufemi <Oyekanmi.Admin@axamansard.com>
2021-03-03 11:44:35 +0100

    added main tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
+variable "rg" {
+  default = "terraform-lab2"
+}
+
 variable "loc" {
   description = "Default Azure region"
   default = "West Europe"
 }
 
 variable "tags" {
+  type = "map"
   default = {
       source = "citadel"
       env = "training"
